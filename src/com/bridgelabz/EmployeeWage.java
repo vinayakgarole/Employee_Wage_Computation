8b6ce10c6a920d27147046fb4fb4e51d92b0bf99
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-08 12:51:57 +0530

    Added: Employee daily wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,12 +5,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int dailyWage = 0;
         int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
 
         if (employeeCheck == 1) {
+            dailyWage = fullDayHour * wagePerHour;
             System.out.println("Employee is present");
         } else {
             System.out.println("Employee is absent");
         }
+        System.out.println("Employee total wage is: " +dailyWage);
     }
 }

f40dc4e9fd96a8202c8aaa1c4d9ebd4e8aac9f8c
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-08 13:17:43 +0530

    Added: Employee wage month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -8,22 +8,25 @@ public class EmployeeWage {
         int wagePerHour = 20;
         int fullTimeHour = 8;
         int partTimeHour = 4;
+        int workingDays = 20;
         int dailyWage = 0;
 
-        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+        for (int day=0; day<=workingDays; day++) {
+            int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
 
-        switch (employeeCheck) {
-            case 2:
-                dailyWage = fullTimeHour * wagePerHour;
-                System.out.println("Employee is present full time");
-                break;
-            case 1:
-                dailyWage = partTimeHour * wagePerHour;
-                System.out.println("Employee is present half time");
-                break;
-            default:
-                System.out.println("Employee is absent");
+            switch (employeeCheck) {
+                case 2:
+                    dailyWage = dailyWage + fullTimeHour * wagePerHour;
+                    System.out.println("Employee is present full time: " +dailyWage);
+                    break;
+                case 1:
+                    dailyWage = dailyWage + partTimeHour * wagePerHour;
+                    System.out.println("Employee is present half time: " +dailyWage);
+                    break;
+                default:
+                    System.out.println("Employee is absent: " +dailyWage);
+            }
         }
-        System.out.println("Employee total wage is: " +dailyWage);
+        System.out.println("Employee total wage for month is: " +dailyWage);
     }
 }

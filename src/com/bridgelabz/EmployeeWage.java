ff5eb726dfd67cec24f218e687da4d71599f3b98
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-08 13:05:48 +0530

    Added: Introduce switch

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -12,14 +12,17 @@ public class EmployeeWage {
 
         int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
 
-        if (employeeCheck == 2) {
-            dailyWage = fullTimeHour * wagePerHour;
-            System.out.println("Employee is present full time");
-        } else if (employeeCheck == 1) {
-            dailyWage = partTimeHour * wagePerHour;
-            System.out.println("Employee is present half time");
-        } else {
-            System.out.println("Employee is absent");
+        switch (employeeCheck) {
+            case 2:
+                dailyWage = fullTimeHour * wagePerHour;
+                System.out.println("Employee is present full time");
+                break;
+            case 1:
+                dailyWage = partTimeHour * wagePerHour;
+                System.out.println("Employee is present half time");
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
         System.out.println("Employee total wage is: " +dailyWage);
     }
